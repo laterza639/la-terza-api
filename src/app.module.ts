f80ef8c17541f3a2adb418ceac4697d3e7a92425
Felix Alejandro Zelaya Orellana <fazogato@gmail.com>
2024-12-21 19:17:20 -0400

    fix variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,11 +20,6 @@ import { ScheduleModule } from './schedule/schedule.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
       ssl: {
